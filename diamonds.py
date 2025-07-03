fbb18bc66858cdc0536564526f831c706c4f80a5
pdhatemwa <patrick.dhatemwa@strathmore.edu>
2025-07-03 16:11:34 +0300

    changes

--- diamonds.py
+++ diamonds.py
@@ -1,1 +1,11 @@
 import pandas as pd 
+import streamlit as st
+
+
+data = r"C:\Users\patri\Downloads\diamonds.csv.zip"
+df = pd.read_csv(data)
+
+
+print(df.head(10))
+print(df.shape)
+print(df.columns)
