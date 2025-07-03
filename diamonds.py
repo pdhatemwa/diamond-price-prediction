941425d522a4e7cddd8cfd680c7ceee10f8a9d16
pdhatemwa <patrick.dhatemwa@strathmore.edu>
2025-07-03 16:27:27 +0300

    yes

--- diamonds.py
+++ diamonds.py
@@ -1,6 +1,5 @@
 import pandas as pd 
 import streamlit as st
-<<<<<<< HEAD
 
 
 data = r"C:\Users\patri\Downloads\diamonds.csv.zip"
@@ -10,6 +9,3 @@ df = pd.read_csv(data)
 print(df.head(10))
 print(df.shape)
 print(df.columns)
-=======
-print("hello")
->>>>>>> 61be8de79a0623100730fda2cb87edeb8a2d1085
