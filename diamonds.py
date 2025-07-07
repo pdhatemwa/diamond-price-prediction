c3f8455167a5bc5dbadbe329def61b1a1a249ae9
pdhatemwa <patrick.dhatemwa@strathmore.edu>
2025-07-07 14:53:13 +0300

    minor changes

--- diamonds.py
+++ diamonds.py
@@ -1,6 +1,13 @@
 import pandas as pd 
 import streamlit as st
 
-data = r"C:\Users\patri\OneDrive\Documents\diamonds.csv"
+# 1. a) Load the dataset.
+data = r"C:\Users\patri\Downloads\diamonds.csv.zip" # I've used an absolute path. Use relative path to access the data.
+df = pd.read_csv(data) # Convert csv into dataframe using pandas.
 
-df = pd.read_csv(data)
+
+print(df.head(10)) # The data looks good
+print(df.shape) # (53940 rows, 11 columns) Good data to work with
+print(df.columns) #['Unnamed: 0', 'carat', 'cut', 'color', 'clarity', 'depth', 'table','price', 'x', 'y', 'z']
+
+# 1. b) Explanatory data analysis.
